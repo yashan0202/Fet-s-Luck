af66a46ab5c366c6950f9e4b65100c415f8910f3
Yash <yashuan0202@gmail.com>
2024-11-09 09:14:26 +0530

    Add files via upload

---
+++ js/auto_type.js
@@ -0,0 +1,9 @@
+
+var options = {
+  strings: ["Love at first chat", "100% real 0% catfish"],
+  typeSpeed: 70,
+  backSpeed: 30,
+  loop: true,
+};
+
+var typed = new Typed(".slogan-app", options); 
